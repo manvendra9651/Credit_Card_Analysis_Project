5f3de36c204c99a5b04b7a9bc28118d5cd2bf9ae
MANVENDRA KUMAR <manvendra9651@gmail.com>
2026-05-06 16:03:43 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.read_csv("creditcard.csv")
+
+st.title("💳 Credit Card Fraud Analysis")
+
+# Show data
+if st.checkbox("Show Data"):
+    st.write(df.head())
+
+# Class distribution
+st.subheader("Fraud vs Normal")
+st.bar_chart(df["Class"].value_counts())
+
+# Pivot table
+st.subheader("Pivot Table (Amount vs Class)")
+pivot = df.pivot_table(values="Amount", index="Class", aggfunc="mean")
+st.write(pivot)
+
+# Amount filter
+amount = st.slider("Select Amount", 0, 5000, 1000)
+st.write(df[df["Amount"] > amount])
